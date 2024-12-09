729d0156b68c9f3fd17ed655760f581b65a023c5
TheDavisWIN <davishuynh1148@gmail.com>
2024-12-08 22:46:26 -0600

    Multiplayer Card Game Framework

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { UI } from "./components/UI";
+import { Leva } from "leva";
+import { isHost, isStreamScreen } from "playroomkit";
+import { MotionConfig } from "framer-motion";
+
+const DEBUG = true;
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <>
+      <Leva hidden={!DEBUG || !isHost()} />
+      <Canvas 
+        shadows
+        camera={{
+          position: isStreamScreen() ? [14, 10, -14] : [0, 4, 12],
+          fov: 30,
+        }}
+      >
+        <color attach="background" args={["#ececec"]} />
+        <MotionConfig
+          transition={{
+            type: "spring",
+            mass: 5,
+            stiffness: 500,
+            damping: 100,
+            restDelta: 0.0001,
+          }}
+        >
+          <Experience />
+        </MotionConfig>
+      </Canvas>
+      <UI />
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { GameEngineProvider } from './hooks/useGameEngine'
+import { insertCoin } from 'playroomkit'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+insertCoin({
+  streamMode: true,
+}).then(() => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <GameEngineProvider>
+        <App />
+      </GameEngineProvider>
+    </React.StrictMode>,
+  )
+});
